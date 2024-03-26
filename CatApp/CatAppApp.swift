ef5e86b3c88a3d1968db179ceb555fd97ae04e89
Sebastian Koller <sebastian@bookkeepr.app>
2024-03-26 17:55:56 +0100

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by Sebastian Koller on 26.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
